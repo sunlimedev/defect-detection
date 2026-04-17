d676030bb3d4833baa22a963d3be5a9d4451dbf8
sunlime <79546567+sunlimedev@users.noreply.github.com>
2026-04-17 15:20:23 -0400

    working code

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import gpiozero
-import cv2
 import os
 import time
 from evdev import InputDevice, ecodes
@@ -21,8 +20,8 @@ start_btn = gpiozero.Button(13)
 flip_btn = gpiozero.Button(19)
 stop_btn = gpiozero.Button(26)
 
-RETRY_DELAY = config.barcode_scanner.retry_delay
-SCANNER_DEVICE_ID = config.barcode_scanner.device_id
+RETRY_DELAY = 5.0
+SCANNER_DEVICE_ID = "/dev/input/by-id/usb-NetChip_HID_Gadget_20080411-event-kbd"
 
 def read_barcode(device_path=SCANNER_DEVICE_ID):
     """Read barcode from the specified input device. Blocking until barcode is read. Retries if the device is unavailable."""
@@ -56,12 +55,53 @@ def read_barcode(device_path=SCANNER_DEVICE_ID):
             time.sleep(RETRY_DELAY)
 
 
+import smtplib
+import ssl
+from email.message import EmailMessage
+import os
+EMAIL_SENDER = "part.defect.notification@gmail.com"
+EMAIL_PASSWORD = "zvod nwrs muxd kidc"
+EMAIL_RECEIVER_LIST = ["apr6001@psu.edu"]
+INCLUDE_PICTURES = True
 
+def send_email(subject,content,pictures=None):
+    """Send an email with the given content to the receiver list in config."""
+
+
+    if not EMAIL_SENDER or not EMAIL_PASSWORD or EMAIL_RECEIVER_LIST == [""]:
+        print("Email config is incomplete. Skipping email sending.")
+        return
+
+    # Create the email message object
+    em = EmailMessage()
+    em.set_content(content)
+    em['Subject'] = subject
+    em['From'] = EMAIL_SENDER
+    em['Bcc'] = ', '.join(EMAIL_RECEIVER_LIST) # Use Bcc to hide recipient list
+
+    if INCLUDE_PICTURES and pictures:
+        for picture in pictures:
+            try:
+                with open(picture, 'rb') as img:
+                    img_data = img.read()
+                    em.add_attachment(img_data, maintype='image', subtype='jpeg', filename=os.path.basename(picture))
+            except Exception as e:
+                print(f"Error attaching picture {picture}: {e}")
+
+    # Connect to the SMTP server and send
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
+            smtp.send_message(em)
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Email Error: {e}")
 
 
 
 # wait for barcode scan and start signal
-serial_number1 = read_barcode(SCANNER_DEVICE_ID)
+serial_number1 = read_barcode()
 
 # run through 3 positions for part 1 (pulse 101)
 start_led.blink()
@@ -70,31 +110,31 @@ while not start_btn.is_held:
 start_led.off()
 prog1_led.on()
 
-relay_start.on()
+relay_pins[0].on()
 time.sleep(0.02)
-realy_start.off()
-time.sleep(10)
+relay_pins[1].off()
+time.sleep(8)
 
-for _ in range(3):
-    relay_next.on()
+for _ in range(6):
+    relay_pins[1].on()
     time.sleep(0.02)
-    relay_next.off()
-    time.sleep(10)
+    relay_pins[1].off()
+    time.sleep(8)
 
 # wait for flip button
 flip_led.blink()
-while not flip_btn.is_help:
+while not flip_btn.is_held:
     continue
 prog1_led.off()
 flip_led.off()
 prog2_led.on()
 
 # run through last three positions for part 1
-for _ in range(3):
-    relay_next.on()
+for _ in range(6):
+    relay_pins[1].on()
     time.sleep(0.02)
-    relay_next.off()
-    time.sleep(10)
+    relay_pins[1].off()
+    time.sleep(8)
 
 # first part good
 prog2_led.off()
@@ -102,13 +142,12 @@ good_led.on()
 
 
 
-# update csv with data and send email (good part)
-#send_email(serial_number1, 0, "no_defect")
+# update csv with data 
 
 
 
 # wait for barcode scan and start signal
-serial_number2 = read_barcode(SCANNER_DEVICE_ID)
+serial_number2 = read_barcode()
 start_led.blink()
 while not start_btn.is_held:
     continue
@@ -117,9 +156,9 @@ start_led.off()
 prog1_led.on()
 
 # go to position 1
-relay_start.on()
+relay_pins[1].on()
 time.sleep(0.02)
-relay_start.off()
+relay_pins[1].off()
 time.sleep(7)
 prog1_led.off()
 defective_led.on()
@@ -127,12 +166,13 @@ defective_led.on()
 
 
 # update csv with defect and send email (squircle discoloration)
-#send_email(serial_number2, 1, "discoloration")
+content = f"Defect detected in squircle part {serial_number2} - discoloration."
+send_email(f"Defect Detected: {serial_number2}", content, None)
 
 
 
 # wait for barcode scan and start signal
-serial_number3 = read_barcode(SCANNER_DEVICE_ID)
+serial_number3 = read_barcode()
 start_led.blink()
 while not start_btn.is_held:
     continue
@@ -141,9 +181,9 @@ start_led.off()
 prog1_led.on()
 
 # go to position 1
-relay_start.on()
+relay_pins[1].on()
 time.sleep(0.02)
-relay_start.off()
+relay_pins[1].off()
 time.sleep(7)
 prog1_led.off()
 defective_led.on()
@@ -151,12 +191,13 @@ defective_led.on()
 
 
 # update csv with defect and send email (rectangle scratch)
-#send_email(serial_number3, 1, "scratch")
+content = f"Defect detected in rectangle part {serial_number3} - scratch."
+send_email(f"Defect Detected: {serial_number3}", content, None)
 
 
 
 # wait for barcode scan and start signal
-serial_number4 = read_barcode(SCANNER_DEVICE_ID)
+serial_number4 = read_barcode()
 start_led.blink()
 while not start_btn.is_held:
     continue
@@ -165,9 +206,9 @@ start_led.off()
 prog1_led.on()
 
 # go to position 1
-relay_start.on()
+relay_pins[1].on()
 time.sleep(0.02)
-relay_start.off()
+relay_pins[1].off()
 time.sleep(7)
 prog1_led.off()
 defective_led.on()
@@ -175,4 +216,8 @@ defective_led.on()
 
 
 # update csv with defect and send email (square dent)
-#send_email(serial_number4, 1, "dent")
+content = f"Defect detected in square part {serial_number4} - dent."
+send_email(f"Defect Detected: {serial_number4}", content, None)
+
+
+time.sleep(100)
